634cd33b6967637c7dd886de92710f7485983667
dsajwan98 <37627908+dsajwan98@users.noreply.github.com>
2019-06-23 13:57:10 +0530

    calling functionality

---
+++ mobile script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $("#response-container").css("display","none");
+    $("#number-area").attr("disabled", "disabled");
+    $('#call-icon').removeAttr("click");
+
+    $('#call-icon').click(function(){
+        if($('#number-area').val().length>0){
+            call();
+            $('#resp-name').html("");
+            $('#resp-number').html($('#number-area').val());
+        }       
+    });
+
+    $('#dialer-table td').click(function(){
+        $('#number-area').val($('#number-area').val()+$(this).html());
+    });
+
+    $('#btnCancel').click(function(){
+        $("#response-container").css("display","none");
+        $("#dialer-container").css("display","block");
+    });
+
+    $('.contact-click').click(function(){
+        call();   
+        var name= $(this).children('span').children('.contact-info-name').text();
+        var number=$(this).children('span').children('.contact-info-number').text();
+        $('#resp-name').html(name);   
+        $('#resp-number').html(number);
+    });
+
+    function call(){
+        $("#response-container").css("display","block");
+        $("#dialer-container").css("display","none");
+    }
+});
